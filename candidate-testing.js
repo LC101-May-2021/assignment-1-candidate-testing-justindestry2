6815ee623f5e727808f30e43e79cdf905910b0c5
justindestry2 <>
2021-06-03 20:48:53 +0000

    finished part 2 - code runs through name, and answers questions with users answers and the tests correct answers

--- candidate-testing.js
+++ candidate-testing.js
@@ -1,3 +1,114 @@
+// const input = require('readline-sync');
+// let candidateName = '';
+
+// // TODO 2: modify your quiz app to ask 5 questions //
+
+
+
+// let candidateAnswer = "";
+// let questions = ["Who was the first American woman in space? ", "True or false: 5 kilometer == 5000 meters? ", "(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "];
+// let correctAnswers = ["Sally Ride", true, 40, "Trajectory", 3];
+// let candidateAnswers;
+
+
+// function askForName() {
+//   // TODO 1.1b: Ask for candidate's name //
+//   candidateName = input.question("Enter your name: ");
+//   console.log(candidateName);
+  
+
+// }
+
+// function askQuestion() {
+//   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
+// // candidateAnswers = questions.length`${1}${2}${3}${4}${5}`
+
+
+// function gradeQuiz(candidateAnswers) {
+
+//   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
+
+// console.log("Your answer: " + candidateAnswer);
+// console.log("Correct answer: " + correctAnswer);
+
+//   let grade;
+  
+  
+
+//   return grade;
+// }
+
+// function runProgram() {
+//   askForName();
+//   // TODO 1.1c: Ask for candidate's name //
+//   console.log("Hello " + candidateName + "" + "!");
+
+
+//   askQuestion();
+//   gradeQuiz(this.candidateAnswers);
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 const input = require('readline-sync');
 
 // TODO 2: modify your quiz app to ask 5 questions //
@@ -7,12 +118,12 @@ let candidateName = '';
 
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 
-let question;
-let correctAnswer;
-let candidateAnswer;
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let question = "";
+let correctAnswer = "";
+let candidateAnswer = "";
+let questions = [ "1) Who was the first American woman in space? ", "2) True or false: 5 kilometer == 5000 meters? ", "3) (5 + 3)/2 * 10 = ? ", "4) Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "5) What is the minimum crew size for the ISS? "];
+let correctAnswers = ["Sally Ride", true, 40, "Trajectory", 3];
+let candidateAnswers = "";
 
 
 function askForName() {
@@ -25,16 +136,28 @@ function askForName() {
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
+// candidateAnswer = input.question(question);
 
+for (let i = 0; i < questions.length; i ++){
+  candidateAnswers = input.question(console.log(questions[i]));
+
+console.log(`Your Answer:  ${candidateAnswers}
+Correct Answer: ${correctAnswers[i]}`)
+}
 
 }
 
+
+
 function gradeQuiz(candidateAnswers) {
+
+
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
 
 
   let grade;
+  
   
 
   return grade;
@@ -43,12 +166,30 @@ function gradeQuiz(candidateAnswers) {
 function runProgram() {
   askForName();
   // TODO 1.1c: Ask for candidate's name //
-  console.log("Hello " + candidateName + "" + "!");
+  console.log("Candidate Name: " + candidateName);
 
 
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Don't write any code below this line //
 // And don't change these or your program will not run as expected //
@@ -62,4 +203,4 @@ module.exports = {
   candidateAnswers: candidateAnswers,
   gradeQuiz: gradeQuiz,
   runProgram: runProgram
-};
+}
