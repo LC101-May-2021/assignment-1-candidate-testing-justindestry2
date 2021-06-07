5ef8afc45e0604ddcd7ec0cc27daeb59ef44fa8f
justindestry2 <>
2021-06-07 03:26:40 +0000

    test push

--- candidate-testing.js
+++ candidate-testing.js
@@ -1,114 +1,3 @@
-// const input = require('readline-sync');
-// let candidateName = '';
-
-// // TODO 2: modify your quiz app to ask 5 questions //
-
-
-
-// let candidateAnswer = "";
-// let questions = ["Who was the first American woman in space? ", "True or false: 5 kilometer == 5000 meters? ", "(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "];
-// let correctAnswers = ["Sally Ride", true, 40, "Trajectory", 3];
-// let candidateAnswers;
-
-
-// function askForName() {
-//   // TODO 1.1b: Ask for candidate's name //
-//   candidateName = input.question("Enter your name: ");
-//   console.log(candidateName);
-  
-
-// }
-
-// function askQuestion() {
-//   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-// // candidateAnswers = questions.length`${1}${2}${3}${4}${5}`
-
-
-// function gradeQuiz(candidateAnswers) {
-
-//   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-
-// console.log("Your answer: " + candidateAnswer);
-// console.log("Correct answer: " + correctAnswer);
-
-//   let grade;
-  
-  
-
-//   return grade;
-// }
-
-// function runProgram() {
-//   askForName();
-//   // TODO 1.1c: Ask for candidate's name //
-//   console.log("Hello " + candidateName + "" + "!");
-
-
-//   askQuestion();
-//   gradeQuiz(this.candidateAnswers);
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 const input = require('readline-sync');
 
 // TODO 2: modify your quiz app to ask 5 questions //
@@ -141,7 +30,7 @@ function askQuestion() {
 for (let i = 0; i < questions.length; i ++){
  candidateAnswers[i] = input.question(console.log(questions[i]));
 
-console.log(`Your Answer:  ${candidateAnswers}
+console.log(`Your Answer:  ${candidateAnswers[i]}
 Correct Answer: ${correctAnswers[i]}`)
 }
 
@@ -163,7 +52,7 @@ for (let i=0; i < 5; i++){
 
 
 // grade / questions.length * 100
-console.log(`>>> Overall Grade: ${grade/ questions.length * 100} (${grade} of 5 responses correct) <<<`);
+console.log(`>>> Overall Grade: ${grade / questions.length * 100} (${grade} of 5 responses correct) <<<`);
   if (grade/questions.length * 100 >= 80){
     console.log(">>> Status: PASSED <<<")
   } else {
